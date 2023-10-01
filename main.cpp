4bf776e8321ecf8ddb3aae95b154a9e9ae93a410
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-10-02 02:15:45 +0530

    Added a new index.html file and updated codebase

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #define CROW_ENABLE_SSL
+
 #include "crow.h"
+using namespace std;
+using namespace crow;
 
 int main()
 {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "SSL Testing";
+    CROW_ROUTE(app, "/")
+    ([](const request &req, response &res){
+     ifstream in("public/index.html", ifstream::in);
+     if(in){
+        ostringstream contents;
+        contents << in.rdbuf();
+        in.close();
+        res.write(contents.str());
+     }else{
+        res.write("Not found");
+     }
+      res.end();
     });
 
     app.bindaddr("127.0.0.1")
